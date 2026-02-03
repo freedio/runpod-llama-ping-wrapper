0c2ca1f9920c57a8b310c0f42d42b7de460e3089
dio <dom@coradec.com>
2026-02-03 16:43:59 +0100

    Fix llama-server path (/app/llama-server)

--- start.sh
+++ start.sh
@@ -1,7 +1,6 @@
 #!/bin/sh
 set -eu
 
-: "${PORT:=8080}"
 : "${PORT_HEALTH:=8081}"
 
 # Serve /ping with HTTP 200 on the health port
@@ -10,4 +9,4 @@ printf "OK\n" > /pingroot/ping
 /bin/busybox httpd -f -p "${PORT_HEALTH}" -h /pingroot &
 
 # Start llama-server on the main port; your template dockerStartCmd provides the args
-exec llama-server --host 0.0.0.0 --port "${PORT}" "$@"
+exec /app/llama-server "$@"
